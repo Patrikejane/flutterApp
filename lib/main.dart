11fa46aebc12dc0f7579e900b3f359259ed48d9b
Sunimal Malkakulage <sskmal@gmail.com>
2020-09-02 01:49:16 +0900

    word english

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   
   Widget build(BuildContext context){
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome To Flutter',
       home:Scaffold(
@@ -12,7 +14,8 @@ class MyApp extends StatelessWidget{
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          // child: Text('Hello World'),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
